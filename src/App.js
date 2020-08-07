28bfcd349489465994053a8153d34a90d4f9e5a1
klaudiusz.fraj@gmail.com <PGRWgdo7>
2020-08-07 14:20:28 +0200

    work fetch from api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-const App = ()=>{
+const App = () => {
     const APP_ID = '0bbf8ae4';
-    const APP_KEY = '87e529f9c351026777db3caf7f5f4234';
+    const APP_KEY = '87e529f9c351026777db3caf7f5f4234 ';
+
+
+    const [counter, setCounter] = useState(0);
+    useEffect(() => {
+        getRecipes();
+    }, [])
+
+    const getRecipes = async () => {
+        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        console.log(response);
+        const data = await response.json();
+        console.log(data.hits);
+    }
 
     return (
         <div className='App'>
-            <h1>hellllo klud</h1>
+            <form className='search-form'>
+                <input className='search-bar' type="text"/>
+                <button className='search-btn'>Search</button>
+            </form>
         </div>
     )
 }
