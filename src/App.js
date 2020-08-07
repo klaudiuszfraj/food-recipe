582ab4549d39fd8c903d210023b3e61874ea71e0
klaudiusz.fraj@gmail.com <PGRWgdo7>
2020-08-07 10:42:12 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = ()=>{
+    const APP_ID = '0bbf8ae4';
+    const APP_KEY = '87e529f9c351026777db3caf7f5f4234';
+
+    return (
+        <div className='App'>
+            <h1>hellllo klud</h1>
+        </div>
+    )
+}
+
+export default App;
