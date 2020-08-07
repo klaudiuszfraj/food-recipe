badd90a87e4f02bd0de55fdc88f221524b370689
klaudiusz.fraj@gmail.com <PGRWgdo7>
2020-08-07 15:03:34 +0200

    add search functionality

--- src/App.js
+++ src/App.js
@@ -9,22 +9,32 @@ const App = () => {
 
 
     const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState('');
+    const [query, setQuery] = useState('chicken');
 
     useEffect(() => {
         getRecipes();
-    }, [])
+    }, [query ])
 
     const getRecipes = async () => {
-        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
         const data = await response.json();
         console.log(data.hits);
         setRecipes(data.hits);
     }
+    const updateSearch = e => {
+        setSearch(e.target.value);
+    }
+    const getSearch = e=>{
+        e.preventDefault();
+        setQuery(search);
+        setSearch('');
+    }
 
     return (
         <div className='App'>
-            <form className='search-form'>
-                <input className='search-bar' type="text"/>
+            <form onSubmit={getSearch} className='search-form' >
+                <input className='search-bar' type="text" value={search} onChange={updateSearch} />
                 <button className='search-btn'>Search</button>
             </form>
             {recipes.map((recipe, index) => <Recipe key={index}
