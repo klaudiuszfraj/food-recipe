aa6a00682e68604446ac61af31264aff92b52788
klaudiusz.fraj@gmail.com <PGRWgdo7>
2020-08-08 10:48:53 +0200

    add toggle arrow for ingredients

--- src/Ingerdients.js
+++ src/Ingerdients.js
@@ -3,18 +3,25 @@ import style from './ingredinets.module.css'
 
 
 const Ingredients = ({ingredients}) => {
-    const [show, setShow] = useState(style.hide)
+    const [show, setShow] = useState(style.hide);
+    const [arrow, setArrow] = useState('');
 
-    const showList = ()=>{
-        if (show === style.hide){
+    const showList = () => {
+        if (show === style.hide) {
             setShow(style.show)
-        }else {
+            setArrow(style.reverseArrow)
+        } else {
             setShow(style.hide)
+            setArrow('')
         }
     }
+    const arrowClasses = `${arrow} ${style.arrow}`
 
     return (<>
-            <h3 onClick={showList}>Ingredients:</h3>
+            <div className={style.header} onClick={showList}>
+                <h3>Ingredients:</h3>
+                <div className={arrowClasses}></div>
+            </div>
             <ol className={show}>
                 {ingredients.map((ingredient, index) => <li key={index}>{ingredient.text}</li>)}
             </ol>
