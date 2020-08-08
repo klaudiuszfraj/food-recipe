45fcd83c3a8765e7078be53cb04927558dbc6c82
klaudiusz.fraj@gmail.com <PGRWgdo7>
2020-08-08 10:15:34 +0200

    add hide show for ingredients

--- src/Ingerdients.js
+++ src/Ingerdients.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import style from './ingredinets.module.css'
 
 
 const Ingredients = ({ingredients}) => {
+    const [show, setShow] = useState(style.hide)
+
+    const showList = ()=>{
+        if (show === style.hide){
+            setShow(style.show)
+        }else {
+            setShow(style.hide)
+        }
+    }
+
     return (<>
-            <h3>Ingredients:</h3>
-            <ol className={style.ingredientsList}>
+            <h3 onClick={showList}>Ingredients:</h3>
+            <ol className={show}>
                 {ingredients.map((ingredient, index) => <li key={index}>{ingredient.text}</li>)}
             </ol>
         </>
